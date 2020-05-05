7cae72c7e99c6726c14b0b4fd6bfd0be5b70d8f3
adhullip <adhullip@thoughtworks.com>
2020-05-04 20:13:06 -0400

    make first pie chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Pie } from './Pie';
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch("https://data.cdc.gov/resource/pj7m-y5uh.json")
+    .then(r => r.json())
+    .then(j => {
+      
+      console.log(j)
+
+      const cdcRow = j.find((d: any) => 
+        d.state === "United States" && d.indicator === "Weighted distribution of population (%)")
+      const data = toPieShape(cdcRow)
+      
+      setData(data)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p>If you want to understand modern racism, consider that Covid-19 is totally color blind and yet, it disproportionately murders people of color in the United States. Sadly of course, millions of white people have lost their lives to the virus too. In fact non-hispanic whites make up the most deaths according to the CDC.</p>
+      <Pie radius={300} data={data} />
+      <p>So where's the racism? Well the key word is <em>disproportionate</em>. If white people make up 50% of the population, they should make up 50% of the deaths. Thats not the case. This is the breakdown of demographics in the United States:</p>
     </div>
   );
 }
 
+const toPieShape = (cdcRow: any) : any => Object.keys(cdcRow)
+  .filter(k => k !== "state" && k !== "indicator" && k !== "data_as_of")
+  .map((k) => ({name: cdcToPresentation[k], ratio: cdcRow[k]/100}))
+
 export default App;
+
+const cdcToPresentation: any = {
+  non_hispanic_white: "White",
+  non_hispanic_black_african_american: "Black / African American",
+  non_hispanic_american_indian_alaska_native: "American Indian / Alaska Native",
+  non_hispanic_asian_pacific_islander: "Asian",
+  hispanic_latino_total: "Latinx",
+  other: "Other"
+}
+
---
+++ src/Pie.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+
+export function Pie(props: PropTypes) {
+
+  const colors = ["coral", "cornflowerblue", "darkgoldenrod", "crimson", "darkslateblue", "darkcyan "];
+
+  const sliceViewModels = props.data
+    .reduce((a: Array<SliceViewModel>, s: Slice) => {
+      const last = a[a.length - 1];
+      const sliceCirc = s.ratio * props.radius * Math.PI;
+      return a.concat({
+        circOffset: last.circOffset + last.circSlice,
+        circSlice: sliceCirc,
+        label: s.name,
+        color: colors[a.length - 1]
+      });
+    }, [{ circOffset: 0, circSlice: 0, label: '', color: "" }]);
+
+  const slices = sliceViewModels.map((m: SliceViewModel) => ([
+    <circle r={props.radius / 2} cx={props.radius} cy={props.radius} fill="transparent" stroke={m.color} strokeWidth={props.radius} strokeDasharray={`0 ${m.circOffset} ${m.circSlice} 500`} />,
+  ]));
+
+  const diameter = props.radius * 2;
+
+  return (<svg width={diameter} viewBox={`-10 -10 ${diameter + 500} ${diameter + 50}`}>
+    <circle r={props.radius} cx={props.radius} cy={props.radius} fill="gray" stroke="black" strokeWidth={10} />
+    {slices}
+    <Legend left={diameter}  sliceViewModels={sliceViewModels} />
+  </svg>);
+
+
+}
+
+interface SliceViewModel {
+    circOffset: number;
+    circSlice: number;
+    label: string;
+    color: string;
+}
+
+interface Slice {
+  name: string;
+  ratio: number;
+}
+
+interface PropTypes {
+    radius: number;
+    data: Array<Slice>;
+}
+
+function Legend(props: {left: number, sliceViewModels: SliceViewModel[]}) {
+  function toLegendLabel(a: any[], m: SliceViewModel): any[] {
+    const last = a[a.length - 1];
+    const lastHeight = !last ? 0 : last.y + (44 * Math.ceil(last.label.split(" ").length / 3));
+
+    return a.concat({ ...m, x: props.left, y: lastHeight });
+  }
+  
+  const legend = props.sliceViewModels
+    .reduce(toLegendLabel, [])
+    .map((m: any) => <LegendLabel x={m.x} y={m.y} color={m.color} label={m.label} />);
+  
+    return <React.Fragment>
+      {legend}
+    </React.Fragment>;
+}
+
+
+function LegendLabel(props: {x: number, y: number, color: string, label: string}): JSX.Element {
+  const isMultipleOf3 = (x: number) => {
+    // console.log(x)
+    return x !== 0 && x % 3 === 0
+  }
+  
+  let lines = props.label
+    .split(" ")
+    .reduce((a: string[][], w: string) => {
+      if(isMultipleOf3(last(a).length)) {
+        return a.concat([[w]])
+      }
+
+      return init(a).concat([last(a).concat(w)])
+    }, [[]])
+    .map(lines => lines.join(" "))
+
+  return (
+    <React.Fragment>
+      {lines.map((l, i) => (
+        <text x={props.x + 50} y="35%" className={"Legend-label"} dy={`${props.y + (40 * i)}`} fill={`${props.color}`} stroke={`${props.color}`} text-anchor="left">{`${l}`}</text>
+      ))}
+    </React.Fragment>
+    )
+}
+
+function last<T>(a: T[]): T {
+  return a[a.length - 1]
+} 
+
+function init<T>(a: T[]): T[] {
+  return a.splice(0, a.length - 1) 
+} 
